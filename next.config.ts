434baeb810b1cd546b2e4a5aabf55bf3a52a2446
minhtungo <loducfc@gmail.com>
2025-04-08 00:24:22 -0400

    feat: add Payload CMS admin and API routes with initial layout configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: false,
+  },
 };
 
 export default withPayload(nextConfig);
